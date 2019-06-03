bb29f03fd86e9346f9e4625a0aa901ae1f1cf520
Anna Yovandich <anna+test@stackery.io>
2019-06-03 21:41:15 +0000

    Modified 2 resources:
    Updated function resource Hi
    Updated function resource Echo

--- Stackery/functions/Hi/index.js
+++ Stackery/functions/Hi/index.js
@@ -1,25 +1,5 @@
-module.exports = function handler(request, output) {
-  // Log the request to the console.
-  console.log('Request:')
-  console.dir(request)
-  
-  let responseBody = `
-    <html>
-      <body>
-        <h4>Woot!</h4>
-        <p>Try this: <a href="hi">hi</a>. It will echo "hi" back to you!</p>
-      </body>
-    </html>
-  `
+module.exports = async message => {
+  console.dir(message);
 
-  // Build an HTTP response.
-  let response = {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "text/html"
-    },
-    body: responseBody
-  }
-  
-  return response
+  return {};
 }
